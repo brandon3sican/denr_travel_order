8c0b1370f57041d1d44ed418e90024118814675a
brandon3sican <esican123@gmail.com>
2025-08-02 23:50:11 +0800

    Add jQuery and Select2 libraries to app layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -53,6 +53,8 @@
         </div>
         @yield('content')
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/table-filters.js') }}"></script>
 </body>
